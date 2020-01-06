ad34c2f01c37bfa32cd6e58f137b8dd989a8a523
Aftermoon-dev <ddol0225@gmail.com>
2020-01-07 02:19:40 +0900

    Apply Korean in Element UI

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+import Element from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import locale from 'element-ui/lib/locale/lang/ko'
+import Storage from 'vue-web-storage'
+
+Vue.use(Element, { locale })
+Vue.use(Storage);
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
--- src/popup/router/index.js
+++ src/popup/router/index.js
@@ -2,15 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import routes from './routes'
 
-import Element from 'element-ui'
-import 'element-ui/lib/theme-chalk/index.css'
-import ElementLang from 'element-ui/lib/locale/lang/ko'
-
-import Storage from 'vue-web-storage'
-
 Vue.use(VueRouter)
-Vue.use(Element, {ElementLang})
-Vue.use(Storage);
 
 export default new VueRouter({
   routes
